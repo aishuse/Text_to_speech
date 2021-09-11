9706ed33626816d8493fedf8df818c31b17b19fc
aiswarya <aiswarya180@gmail.com>
2021-09-11 16:58:20 +0530

    1st commit

---
+++ multiplication.py
@@ -0,0 +1,26 @@
+
+import pyttsx3
+n=input('enter a number')
+dic={
+    "1":"ones",
+    "2":"twos",
+    "3":"threes",
+    "4":"fours",
+    "5":"fives",
+    "6":"sixs",
+    "7":"sevens",
+    "8":"eights",
+    "9":"nines",
+    "10":"tens"
+
+}
+
+txt=""
+for i in range(1,11):
+    mul=i*int(n)
+    txt+=str(i)+" "+dic[n]+" are "+str(mul)+"\n"
+print(txt)
+
+engine=pyttsx3.init()
+engine.say(txt)
+engine.runAndWait()
